497ffd3087770341fbd75d0011c0f91683ad4527
Renato Lerac <renatolerac@gmail.com>
2013-01-17 12:17:40 -0200

    Separa conteúdo do pdf em dias

--- app/controllers/parser_controller.rb
+++ app/controllers/parser_controller.rb
@@ -17,6 +17,22 @@ class ParserController < ActionController::Base
 
     File.delete full_path
 
+    @output = parse @output
+
     render 'index'
+  end
+
+    DAYS_SPLIT = /(Sun|Mon|Tue|Wed|Thu|Fri|Sat)(\d{2})(.*?)(?=(Sun|Mon|Tue|Wed|Thu|Fri|Sat|\Z))/
+
+  def parse (string)
+    final_strings = []
+    string.gsub(/\n+/, "").scan(DAYS_SPLIT) do |m|
+      split = ""
+      split.concat $1
+      split.concat $2
+      split.concat $3
+      final_strings.push split
+    end
+    final_strings
   end
 end
